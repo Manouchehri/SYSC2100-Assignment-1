af37f1604f080b7e66379907c5be8efd9081d049
David Manouchehri <manouchehri@riseup.net>
2016-01-24 21:37:48 +0000

    Added some comments.

--- src/ConfigurationCounting.java
+++ src/ConfigurationCounting.java
@@ -9,18 +9,21 @@ public class ConfigurationCounting {
     }
 
     private static void configs(int numOfChannels, int numOfNodes, int currentChannel, int[] currentLayout, ArrayList<int[]> list) {
+        /* Have all the nodes been assigned yet? */
         if (numOfNodes <= 0) {
-            list.add(currentLayout);
-        } else {
-            int[] testLayout = new int[currentLayout.length];
-            System.arraycopy(currentLayout, 0, testLayout, 0, currentLayout.length);
-            if (currentChannel < numOfChannels) {
-                testLayout[currentChannel]++;
-                configs(numOfChannels, numOfNodes, ++currentChannel, currentLayout, list);
-            } else {
-                return;
+            list.add(currentLayout); /* This is a valid configuration, add it to the list. */
+        }
+        else { /* There's still nodes left to assign. */
+            int[] testLayout = new int[currentLayout.length]; /* Create a holder array. */
+            System.arraycopy(currentLayout, 0, testLayout, 0, currentLayout.length); /* Copy the array. */
+            if (currentChannel < numOfChannels) { /* Have we gone beyond the number of channels left? */
+                testLayout[currentChannel]++; /* Add a node to the current channel. */
+                configs(numOfChannels, numOfNodes, ++currentChannel, currentLayout, list); /* Move onto the next channel. */
             }
-            configs(numOfChannels, --numOfNodes, 0, testLayout, list);
+            else { /* We've ran out of channels and can't assign a node. */
+                return; /* Throw testLayout away. */
+            }
+            configs(numOfChannels, --numOfNodes, 0, testLayout, list); /* testLayout might be valid, continue to check. */
         }
     }
 

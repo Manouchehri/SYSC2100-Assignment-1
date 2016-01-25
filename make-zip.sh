2abcbce06dffde1a9132037e5e4e8ed9c0dc5ba4
David Manouchehri <manouchehri@riseup.net>
2016-01-25 03:15:56 +0000

    Keep links inside zip.

--- make-zip.sh
+++ make-zip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -9r "$(printf "r%s.%s - %s.zip" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)" "$(date --utc --iso-8601=seconds | sed s/:/./g)")" src/ out/ *.txt *.md *.java *.class
+zip -9ry "$(printf "r%s.%s - %s.zip" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)" "$(date --utc --iso-8601=seconds | sed s/:/./g)")" src/ out/ *.txt *.md *.java *.class
